de5580866ef42695bca66de12d332bdd2830a3cb
Yashik Kaushal <93261243+Yashik9272@users.noreply.github.com>
2024-02-17 15:32:07 +0530

    Add files via upload

---
+++ workout/backend/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const punycode = require('punycode/');
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workout')
+const userRoutes = require('./routes/user')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+// connect to db
+mongoose.connect("mongodb://localhost:27017/admin")
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
